943556b69307b2d46cf2e071264bf5f0fb00374f
andig <cpuidle@gmail.com>
2026-07-05 16:29:58 +0200

    ship: use if err := pattern in sendSpineData

--- ship/connection_serialization.go
+++ ship/connection_serialization.go
@@ -67,8 +67,7 @@ func (c *ShipConnection) sendSpineData(data []byte) error {
 	shipMsg := []byte{model.MsgTypeData}
 	shipMsg = append(shipMsg, eebusMsg...)
 
-	err = c.dataWriter.WriteMessageToWebsocketConnection(shipMsg)
-	if err != nil {
+	if err := c.dataWriter.WriteMessageToWebsocketConnection(shipMsg); err != nil {
 		logging.Log().Debug("error sending message: ", err)
 		// tear down so the SKI is freed for reconnect instead of a zombie writer
 		c.CloseConnection(false, 0, "")
